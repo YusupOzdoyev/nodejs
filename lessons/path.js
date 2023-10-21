f66256b31d1167ee7c7f01a035764f8aef03e2e7
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2023-10-22 02:46:31 +0300

    meet unit testings

---
+++ lessons/path.js
@@ -0,0 +1,10 @@
+// // const path = require('path');
+
+// // console.log('Склеить участки пути', path.join(__filename,'first', 'second'));
+// // const fullpath = path.resolve(__dirname, 'first', 'second', 'third');
+// // console.log('Парсинг пути', path.parse(fullpath));
+
+
+// const siteURL = 'http://localhost:8080/users?id=5123'
+// const url = new URL (siteURL);
+// console.log(url);
